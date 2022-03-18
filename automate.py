59b4b4addc1085155f5fcd346014810e73a126bc
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-03-18 14:56:28 +0700

    Create automate.py

---
+++ automate.py
@@ -0,0 +1,13 @@
+import webbrowser
+
+url = ["https://xsmb.me", "https://xsmn.me","https://xsmb.co","https://xsmn.com","https://xsmn.info"]
+
+brave_path = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
+
+webbrowser.register("brave", None, webbrowser.BackgroundBrowser(brave_path))
+
+webbrowser.get("brave").open(url[0])
+webbrowser.get("brave").open(url[1])
+webbrowser.get("brave").open(url[2])
+webbrowser.get("brave").open(url[3])
+webbrowser.get("brave").open(url[4])
