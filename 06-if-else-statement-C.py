23942b7023927c574aeb57275bfaf83c5a7524de
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-03-21 13:26:54 +0700

    Create 06-if-else-statement-C.py

---
+++ 06-if-else-statement-C.py
@@ -0,0 +1,11 @@
+e = 20
+f = 8
+if e < f:
+    print("e is less than f")
+
+elif e == f:
+    print("e is equal to f")
+elif e > f + 10:
+    print("e is greater than f by more than 10")
+else:
+    print("e is greater than f")
