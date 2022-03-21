60e1555e7adff2b3611ee095b5b8063c352e015c
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-03-21 11:39:33 +0700

    Update brave_automate.py

--- brave_automate.py
+++ brave_automate.py
@@ -1,5 +1,6 @@
 import webbrowser
 
+
 url = ["https://xsmb.me", "https://xsmn.me","https://xsmb.co","https://xsmn.com","https://xsmn.info"]
 
 brave_path = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
@@ -11,5 +12,6 @@ webbrowser.get("brave").open(url[1])
 webbrowser.get("brave").open(url[2])
 webbrowser.get("brave").open(url[3])
 webbrowser.get("brave").open(url[4])
+
 
 
