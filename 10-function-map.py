92fd45a88b225c66898a018f276f03ff3db3a692
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-03-22 14:36:04 +0700

    Create 10-function-map.py

---
+++ 10-function-map.py
@@ -0,0 +1,17 @@
+# a mapping
+# input or an argument
+def function2(x):
+    return 2 * x
+
+
+a = function2(3)
+# return values or output
+print(a)
+
+b = function2(4)
+print(b)
+
+c = function2(5)
+print(c)
+
+
