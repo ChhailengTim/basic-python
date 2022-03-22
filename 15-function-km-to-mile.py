a5dfa337e792c3e4c342a70da2abf901aea547bb
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-03-22 14:53:18 +0700

    Create 15-function-km-to-mile.py

---
+++ 15-function-km-to-mile.py
@@ -0,0 +1,18 @@
+print("Kilometer convert to mile")
+
+km1 = 1
+km2 = 10
+km3 = 100
+
+
+def convert(km):
+    print(km, "Kilometers")
+    return km * 0.6214
+
+
+result1 = convert(km1)
+print(result1, "miles")
+result2 = convert(km2)
+print(result2, "miles")
+result3 = convert(km3)
+print(result3, "miles")
