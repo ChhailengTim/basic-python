9e3b54ed5db2cbd5fd06ed24054e26883d8403e9
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-03-23 15:26:09 +0700

    Create 16-list.py

---
+++ 16-list.py
@@ -0,0 +1,28 @@
+a = [3, 10, -1]
+
+print(a)
+
+a.append(1)
+
+print(a)
+
+a.append("Hello")
+print(a)
+
+a.append([5, 5])
+print(a)
+
+a.pop()
+print(a)
+
+a.pop()
+print(a)
+
+print(a[0])
+
+print(a[3])
+
+a[0] = 100
+print(a)
+
+
