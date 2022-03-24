da1167b3efcc643ed5ca85c6e5544a01d156beb5
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-03-24 20:59:00 +0700

    Create auto-text-to-crush.py

---
+++ webcheck/auto-text-to-crush.py
@@ -0,0 +1,12 @@
+import pyautogui as pg
+import time
+
+print("Automate text to crush")
+time.sleep(5)
+
+for i in range(50):
+    pg.write("Hey crush, I love you")
+    time.sleep(0.5)
+    pg.press("Enter"
+             "Hey crush, I love you"
+             "Hey crush, I love you")
