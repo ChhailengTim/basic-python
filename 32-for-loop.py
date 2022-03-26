85763ff5e7f920807275fccedc9d0883dcd4983c
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-03-26 11:26:29 +0700

    Create 32-for-loop.py

---
+++ 32-for-loop.py
@@ -0,0 +1,12 @@
+a = ["apple", "banana", "republic"]
+for element in a:
+    print(element)
+
+for i in range(3):  # 0, 1, 2
+    print(i)
+
+for i in range(3):
+    print(a[i])
+
+for i in range(len(a)):
+    print(a[i])
