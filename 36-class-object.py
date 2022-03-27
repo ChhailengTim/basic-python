5373135a46d5a13559fb7971df7786b4918710bb
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-03-27 22:20:26 +0700

    Create 36-class-object.py

---
+++ 36-class-object.py
@@ -0,0 +1,11 @@
+class Robot:
+    def introduce_self(self):
+        print("My name is " + self.name)  # this in java
+
+
+r1 = Robot()
+r1.name = "Tom"
+r1.color = "Red"
+r1.weight = 30
+r1.introduce_self()
+
