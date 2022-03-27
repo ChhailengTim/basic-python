654760ccc1b90b029ea65ca96f9f43d1f6b15e01
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-03-28 00:18:59 +0700

    Update web-ping.py

--- webcheck/web-ping.py
+++ webcheck/web-ping.py
@@ -4,24 +4,36 @@ import gspread
 sa = gspread.service_account()
 sh = sa.open("របាយការវែបសាយ")
 
-# file = open('websites.txt', 'r')
-# Lines = file.readlines()
-#
-# count = 0
-# # Strips the newline character
-#
-#
-#
-# def pingwebsite(host):
-#     response = ping(host)
-#
-#     if response == False:
-#         return "មិនដំណើរការ"
-#     else:
-#         return "ដំណើរការ"
-#
-#
-# for line in Lines:
-#     count += 1
-#     print(count)
-#     print(pingwebsite("{}".format(line.strip())))
+wks = sh.worksheet("Sheet1")
+
+# print("Rows: ", wks.row_count)
+# print("Cols: ", wks.col_count)
+
+# print(wks.acell("B9").value)
+# print(wks.cell(3, 4).value)
+
+# print(wks.get_all_records())
+
+
+file = open('websites.txt', 'r')
+Lines = file.readlines()
+
+count = 0
+
+
+# Strips the newline character
+
+
+def pingwebsite(host):
+    response = ping(host)
+
+    if response == False:
+        return "មិនដំណើរការ"
+    else:
+        return "ដំណើរការ"
+
+
+for line in Lines:
+    count += 1
+    print(count)
+    print(pingwebsite("{}".format(line.strip())))
