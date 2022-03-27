2acee606e583372e1762a8e142d9e7e6ecfec221
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-03-27 22:33:01 +0700

    Update 36-class-object.py

--- 36-class-object.py
+++ 36-class-object.py
@@ -1,16 +1,30 @@
+import self as self
+
+
 class Robot:
+    def __init__(self, name, color, weight):
+        self.name = name
+        self.color = color
+        self.weight = weight
+
     def introduce_self(self):
         print("My name is " + self.name)  # this in java
 
 
-r1 = Robot()
-r1.name = "Tom"
-r1.color = "Red"
-r1.weight = 30
+# r1 = Robot()
+# r1.name = "Tom"
+# r1.color = "Red"
+# r1.weight = 30
+# r1.introduce_self()
+#
+# r2 = Robot()
+# r2.name = "Jerry"
+# r2.color = "blue"
+# r2.weight = 30
+# r2.introduce_self()
+
+r1 = Robot("Tom", "Red", 30)
 r1.introduce_self()
 
-r2 = Robot()
-r2.name = "Jerry"
-r2.color = "blue"
-r2.weight = 30
+r2 = Robot("Jerry", "Blue", 30)
 r2.introduce_self()
