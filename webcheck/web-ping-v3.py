ba920f3be19ddce4a660c5c5f8b30e121601830e
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-03-28 11:55:09 +0700

    Create web-ping-v3.py

---
+++ webcheck/web-ping-v3.py
@@ -0,0 +1,10 @@
+import os
+
+hostname = "www.google.com"  # example
+response = os.system("ping -c 1 " + hostname)
+
+# and then check the response...
+if response == 0:
+    print(hostname, 'is up!')
+else:
+    print(hostname, 'is down!')
