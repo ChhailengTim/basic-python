9c2f2f4f0c6e94001d5d6e11c2590c88a600e6e3
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-03-28 11:55:01 +0700

    Create brave-automate-v2.py

---
+++ webcheck/brave-automate-v2.py
@@ -0,0 +1,22 @@
+import webbrowser
+
+file = open('websites.txt', 'r')
+Line = file.readlines()
+
+count = 0
+
+# url = ["https://xsmb.me", "https://xsmn.me","https://xsmb.co","https://xsmn.com","https://xsmn.info"]
+
+brave_path = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
+
+webbrowser.register("brave", None, webbrowser.BackgroundBrowser(brave_path))
+
+# webbrowser.get("brave").open(url[0])
+# webbrowser.get("brave").open(url[1])
+# webbrowser.get("brave").open(url[2])
+# webbrowser.get("brave").open(url[3])
+# webbrowser.get("brave").open(url[4])
+for line in Line:
+    count += 1
+    web = format(line.strip())
+    webbrowser.get("brave").open(web)
