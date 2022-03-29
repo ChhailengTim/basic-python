32035699dd822138f2572ff2f1202ab85a48cfea
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-03-29 11:24:00 +0700

    Create 39-boolean-B.py

---
+++ 39-boolean-B.py
@@ -0,0 +1,14 @@
+a = 3
+b = 1
+
+if a > b:
+    print("a is greater than b")
+
+if True:
+    print("a is greater than b")
+
+boolean_value = a > b
+print(boolean_value)
+
+if boolean_value:
+    print("a is greater than b")
