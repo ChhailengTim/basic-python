19b9a281589784d0d88fd9a19594dc0994a2cadc
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-03-29 14:42:44 +0700

    Create 46-sets-B.py

---
+++ 46-sets-B.py
@@ -0,0 +1,16 @@
+given_list = [1, 1, 2, 4, 2]
+new_set = set()
+for x in given_list:
+    new_set.add(x)
+    print(new_set)
+
+new_list1=list()
+for x in new_set:
+    new_list1.append(x)
+    print(new_list1)
+
+b=set()
+b.add('apple')
+b.add('banana')
+b.add(1)
+print(b)
