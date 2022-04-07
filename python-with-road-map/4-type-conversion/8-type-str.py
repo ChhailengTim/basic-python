db29457f90428d3441e1f22d0cc8a681ba77d8bf
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-04-07 11:01:40 +0700

    Create 8-type-str.py

---
+++ python-with-road-map/4-type-conversion/8-type-str.py
@@ -0,0 +1,7 @@
+gross_world_product = 84.84
+
+gwp_str = str(gross_world_product)
+
+answer_8 = "In 2018 gross product of the world (GWP) was " + gwp_str + " in trillion US dollars."
+
+print(answer_8)
