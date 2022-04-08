5416e1b4ef93a29fac3f42d89141a4a891241318
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-04-08 14:31:38 +0700

    error function with greet

---
+++ python-with-road-map/6-function/2-funtion.py
@@ -0,0 +1,16 @@
+# function call
+greet("Paul")
+
+
+# function definition
+def greet(name):
+    """
+    This function greets to
+    the person passed in as
+    a parameter
+    :param name: greet
+    :return: name
+    """
+    print("Hello, " + name + ". Good morning!")
+
+# Error: name 'greet' is not defined
