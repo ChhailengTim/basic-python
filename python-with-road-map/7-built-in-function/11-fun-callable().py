0719ffe06b72162e9bb54e498c08d8d0a646151d
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-04-10 22:20:50 +0700

    Create 11-fun-callable().py

---
+++ python-with-road-map/7-built-in-function/11-fun-callable().py
@@ -0,0 +1,46 @@
+# Example 1
+x = 5
+print(callable(x))
+
+
+def testFunction():
+    print("Test")
+
+
+y = testFunction
+print(callable(y))
+
+
+# Example 2
+class Foo:
+    def __call__(self):
+        print('Print Something')
+
+
+print(callable(Foo))
+
+
+# Example 3
+class Foo:
+    def __call__(self):
+        print('Print Something')
+
+
+InstanceOfFoo = Foo()
+
+# Prints 'Print Something'
+InstanceOfFoo()
+
+
+# Example 4
+class Foo:
+    def printLine(self):
+        print('Print Something')
+
+
+print(callable(Foo))
+
+InstanceOfFoo = Foo()
+# Raises an Error
+# 'Foo' object is not callable
+InstanceOfFoo()
