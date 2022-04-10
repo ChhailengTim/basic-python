cd9c2c4bb96b6ac9aa02c8704470b53b03e7ce65
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-04-10 22:20:47 +0700

    Create 10-fun-bytearray().py

---
+++ python-with-road-map/7-built-in-function/10-fun-bytearray().py
@@ -0,0 +1,18 @@
+# Example 1
+string = "Python is interesting."
+
+# string with encoding 'utf-8'
+arr = bytearray(string, 'utf-8')
+print(arr)
+
+# Example 2
+size = 5
+
+arr = bytearray(size)
+print(arr)
+
+# Example 2
+rList = [1, 2, 3, 4, 5]
+
+arr = bytearray(rList)
+print(arr)
