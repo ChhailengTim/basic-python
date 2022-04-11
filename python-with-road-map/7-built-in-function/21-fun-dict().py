453a02949bf8b99ad5471f1183bfdcde53b9c284
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-04-11 23:05:59 +0700

    Create 21-fun-dict().py

---
+++ python-with-road-map/7-built-in-function/21-fun-dict().py
@@ -0,0 +1,10 @@
+numbers1 = dict({'x': 4, 'y': 5})
+print('numbers1 =',numbers1)
+
+# you don't need to use dict() in above code
+numbers2 = {'x': 4, 'y': 5}
+print('numbers2 =',numbers2)
+
+# keyword argument is also passed
+numbers3 = dict({'x': 4, 'y': 5}, z=8)
+print('numbers3 =',numbers3)
