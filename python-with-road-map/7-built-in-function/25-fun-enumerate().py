158c532b5f848c4655e638546f6a9bf979ec6375
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-04-12 21:51:06 +0700

    Create 25-fun-enumerate().py

---
+++ python-with-road-map/7-built-in-function/25-fun-enumerate().py
@@ -0,0 +1,8 @@
+languages = ['Python', 'Java', 'JavaScript']
+
+enumerate_prime = enumerate(languages)
+
+# convert enumerate object to list
+print(list(enumerate_prime))
+
+# Output: [(0, 'Python'), (1, 'Java'), (2, 'JavaScript')]
