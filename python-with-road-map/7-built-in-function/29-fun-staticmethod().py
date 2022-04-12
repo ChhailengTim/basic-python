d47d0927eb6ba3895822ea181e26081ff4a9ab9f
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-04-12 21:51:15 +0700

    Create 29-fun-staticmethod().py

---
+++ python-with-road-map/7-built-in-function/29-fun-staticmethod().py
@@ -0,0 +1,9 @@
+class Mathematics:
+
+    def addNumbers(x, y):
+        return x + y
+
+# create addNumbers static method
+Mathematics.addNumbers = staticmethod(Mathematics.addNumbers)
+
+print('The sum is:', Mathematics.addNumbers(5, 10))
