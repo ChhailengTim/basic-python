6cc08403bbcb95fc36a9c8f2c411752c3b474d7c
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-04-13 11:36:40 +0700

    Create 48-fun-frozenset().py

---
+++ python-with-road-map/7-built-in-function/48-fun-frozenset().py
@@ -0,0 +1,9 @@
+# tuple of vowels
+vowels = ('a', 'e', 'i', 'o', 'u')
+
+fSet = frozenset(vowels)
+print('The frozen set is:', fSet)
+print('The empty frozen set is:', frozenset())
+
+# frozensets are immutable
+fSet.add('v')
