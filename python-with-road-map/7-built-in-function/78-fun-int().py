0662fcc9597a401a02e405df32eb47b2c520c71f
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-04-26 14:55:27 +0700

    Create 78-fun-int().py

---
+++ python-with-road-map/7-built-in-function/78-fun-int().py
@@ -0,0 +1,11 @@
+# binary 0b or 0B
+print("For 1010, int is:", int('1010', 2))
+print("For 0b1010, int is:", int('0b1010', 2))
+
+# octal 0o or 0O
+print("For 12, int is:", int('12', 8))
+print("For 0o12, int is:", int('0o12', 8))
+
+# hexadecimal
+print("For A, int is:", int('A', 16))
+print("For 0xA, int is:", int('0xA', 16))
