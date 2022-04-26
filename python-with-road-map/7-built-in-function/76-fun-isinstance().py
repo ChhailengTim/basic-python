5745f46644b19fd95f89fc3426d7b120c1b1ea70
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-04-26 14:55:22 +0700

    Create 76-fun-isinstance().py

---
+++ python-with-road-map/7-built-in-function/76-fun-isinstance().py
@@ -0,0 +1,18 @@
+numbers = [1, 2, 3]
+
+result = isinstance(numbers, list)
+print(numbers,'instance of list?', result)
+
+result = isinstance(numbers, dict)
+print(numbers,'instance of dict?', result)
+
+result = isinstance(numbers, (dict, list))
+print(numbers,'instance of dict or list?', result)
+
+number = 5
+
+result = isinstance(number, list)
+print(number,'instance of list?', result)
+
+result = isinstance(number, int)
+print(number,'instance of int?', result)
