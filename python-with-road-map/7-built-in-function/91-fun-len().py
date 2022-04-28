b91becc947d3ee64f25bec798f70ecb1f7f30b4a
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-04-28 14:22:22 +0700

    Create 91-fun-len().py

---
+++ python-with-road-map/7-built-in-function/91-fun-len().py
@@ -0,0 +1,11 @@
+testList = []
+print(testList, 'length is', len(testList))
+
+testList = [1, 2, 3]
+print(testList, 'length is', len(testList))
+
+testTuple = (1, 2, 3)
+print(testTuple, 'length is', len(testTuple))
+
+testRange = range(1, 10)
+print('Length of', testRange, 'is', len(testRange))
