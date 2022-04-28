9cc820da62df72d50c371a70d5b0c2cbe33f9176
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-04-28 13:52:20 +0700

    Create 85-fun-list().py

---
+++ python-with-road-map/7-built-in-function/85-fun-list().py
@@ -0,0 +1,14 @@
+# empty list
+print(list())
+
+# vowel string
+vowel_string = 'aeiou'
+print(list(vowel_string))
+
+# vowel tuple
+vowel_tuple = ('a', 'e', 'i', 'o', 'u')
+print(list(vowel_tuple))
+
+# vowel list
+vowel_list = ['a', 'e', 'i', 'o', 'u']
+print(list(vowel_list))
