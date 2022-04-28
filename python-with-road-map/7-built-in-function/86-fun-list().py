d8e82cba691c7711c77828423af04e86cfaf1975
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-04-28 13:52:22 +0700

    Create 86-fun-list().py

---
+++ python-with-road-map/7-built-in-function/86-fun-list().py
@@ -0,0 +1,7 @@
+# vowel set
+vowel_set = {'a', 'e', 'i', 'o', 'u'}
+print(list(vowel_set))
+
+# vowel dictionary
+vowel_dictionary = {'a': 1, 'e': 2, 'i': 3, 'o': 4, 'u': 5}
+print(list(vowel_dictionary))
