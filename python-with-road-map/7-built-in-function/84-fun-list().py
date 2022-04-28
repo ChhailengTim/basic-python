6741af7fa4ac537acf16fda08e92bc4e617ef5b5
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-04-28 13:52:15 +0700

    Create 84-fun-list().py

---
+++ python-with-road-map/7-built-in-function/84-fun-list().py
@@ -0,0 +1,11 @@
+text = 'Python'
+
+# convert string to list
+text_list = list(text)
+print(text_list)
+
+# check type of text_list
+print(type(text_list))
+
+# Output: ['P', 'y', 't', 'h', 'o', 'n']
+#         <class 'list'>
