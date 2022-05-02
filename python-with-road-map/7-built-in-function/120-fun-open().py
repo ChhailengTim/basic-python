3ed61810eec97ddda649cbf3ad3d90b2c13f85b4
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-05-02 14:51:35 +0700

    Create 120-fun-open().py

---
+++ python-with-road-map/7-built-in-function/120-fun-open().py
@@ -0,0 +1,10 @@
+# opens the file in reading mode
+f = open("path_to_file", mode='r')
+
+# opens the file in writing mode
+f = open("path_to_file", mode='w')
+
+# opens for writing to the end
+f = open("path_to_file", mode='a')
+
+f = open("path_to_file", mode='r', encoding='utf-8')
