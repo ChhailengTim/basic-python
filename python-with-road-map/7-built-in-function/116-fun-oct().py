b2207f296bc13d5d4b62dd2d18d06c3349f9de02
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-05-02 14:51:44 +0700

    Create 116-fun-oct().py

---
+++ python-with-road-map/7-built-in-function/116-fun-oct().py
@@ -0,0 +1,12 @@
+class Person:
+    age = 23
+
+    def __index__(self):
+        return self.age
+
+    def __int__(self):
+        return self.age
+
+
+person = Person()
+print('The oct is:', oct(person))
