5433099cc5e4845745066299f95e5f295714068e
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-05-02 14:51:33 +0700

    Create 121-fun-pow().py

---
+++ python-with-road-map/7-built-in-function/121-fun-pow().py
@@ -0,0 +1,11 @@
+# positive x, positive y (x**y)
+print(pow(2, 2))  # 4
+
+# negative x, positive y
+print(pow(-2, 2))  # 4
+
+# positive x, negative y
+print(pow(2, -2))  # 0.25
+
+# negative x, negative y
+print(pow(-2, -2))  # 0.25
