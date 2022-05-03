c30d14a36fb3086cc14cc9880242f4a1f0ff6bed
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-03 23:23:24 +0700

    Create 127-fun-property().py

---
+++ python-with-road-map/7-built-in-function/127-fun-property().py
@@ -0,0 +1,24 @@
+class Person:
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        print('Getting name')
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        print('Setting name to ' + value)
+        self._name = value
+
+    @name.deleter
+    def name(self):
+        print('Deleting name')
+        del self._name
+
+
+p = Person('Adam')
+print('The name is:', p.name)
+p.name = 'John'
+del p.name
