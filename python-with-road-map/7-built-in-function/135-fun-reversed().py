ece8946cbde3aa83f24d24dc5e058f2b1b076d1d
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-11 00:07:19 +0700

    Create 135-fun-reversed().py

---
+++ python-with-road-map/7-built-in-function/135-fun-reversed().py
@@ -0,0 +1,15 @@
+# for string
+seq_string = 'Python'
+print(list(reversed(seq_string)))
+
+# for tuple
+seq_tuple = ('P', 'y', 't', 'h', 'o', 'n')
+print(list(reversed(seq_tuple)))
+
+# for range
+seq_range = range(5, 9)
+print(list(reversed(seq_range)))
+
+# for list
+seq_list = [1, 2, 4, 3, 5]
+print(list(reversed(seq_list)))
