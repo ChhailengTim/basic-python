42464aea9b110e211e5d71846ae76b4d69c29710
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-11 00:07:29 +0700

    Create 140-fun-round().py

---
+++ python-with-road-map/7-built-in-function/140-fun-round().py
@@ -0,0 +1,9 @@
+from decimal import Decimal
+
+# normal float
+num = 2.675
+print(round(num, 2))
+
+# using decimal.Decimal (passed float as string for precision)
+num = Decimal('2.675')
+print(round(num, 2))
