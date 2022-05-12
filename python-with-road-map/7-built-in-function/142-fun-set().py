282e80392b98c72179a369cb67d5a6bdb4b029df
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-12 23:48:06 +0700

    Create 142-fun-set().py

---
+++ python-with-road-map/7-built-in-function/142-fun-set().py
@@ -0,0 +1,14 @@
+# empty set
+print(set())
+
+# from string
+print(set('Python'))
+
+# from tuple
+print(set(('a', 'e', 'i', 'o', 'u')))
+
+# from list
+print(set(['a', 'e', 'i', 'o', 'u']))
+
+# from range
+print(set(range(5)))
