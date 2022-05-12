4e352bb11a51e5e0b8932ca4ac61ae723dad2575
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-12 23:48:08 +0700

    Create 143-fun-set().py

---
+++ python-with-road-map/7-built-in-function/143-fun-set().py
@@ -0,0 +1,9 @@
+# from set
+print(set({'a', 'e', 'i', 'o', 'u'}))
+
+# from dictionary
+print(set({'a': 1, 'e': 2, 'i': 3, 'o': 4, 'u': 5}))
+
+# from frozen set
+frozen_set = frozenset(('a', 'e', 'i', 'o', 'u'))
+print(set(frozen_set))
