e6f27fef0303e37f7b610bba23eb98ae687635fd
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-05-16 21:56:02 +0700

    Create 150-fun-slice().py

---
+++ python-with-road-map/7-built-in-function/150-fun-slice().py
@@ -0,0 +1,13 @@
+# Program to get a substring from the given string
+
+py_string = 'Python'
+
+# stop = 3
+# contains 0, 1 and 2 indices
+slice_object = slice(3)
+print(py_string[slice_object])  # Pyt
+
+# start = 1, stop = 6, step = 2
+# contains 1, 3 and 5 indices
+slice_object = slice(1, 6, 2)
+print(py_string[slice_object])  # yhn
