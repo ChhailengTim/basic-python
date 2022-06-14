1fe0b02cc72c9cd4fcf5246ecbf02990338c674b
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-15 00:51:57 +0700

    Create 157-fun-sorted.py

---
+++ python-with-road-map/7-built-in-function/157-fun-sorted.py
@@ -0,0 +1,11 @@
+# vowels list
+py_list = ['e', 'a', 'u', 'o', 'i']
+print(sorted(py_list))
+
+# string
+py_string = 'Python'
+print(sorted(py_string))
+
+# vowels tuple
+py_tuple = ('e', 'a', 'u', 'o', 'i')
+print(sorted(py_tuple))
