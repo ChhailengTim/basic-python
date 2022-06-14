d0a5624fd8ca5b8cb25a8ff0acfe1bf8477c4300
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-15 00:51:58 +0700

    Create 158-fun-sorted.py

---
+++ python-with-road-map/7-built-in-function/158-fun-sorted.py
@@ -0,0 +1,11 @@
+# set
+py_set = {'e', 'a', 'u', 'o', 'i'}
+print(sorted(py_set, reverse=True))
+
+# dictionary
+py_dict = {'e': 1, 'a': 2, 'u': 3, 'o': 4, 'i': 5}
+print(sorted(py_dict, reverse=True))
+
+# frozen set
+frozen_set = frozenset(('e', 'a', 'u', 'o', 'i'))
+print(sorted(frozen_set, reverse=True))
