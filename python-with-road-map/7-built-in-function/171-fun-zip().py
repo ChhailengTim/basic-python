27ecbeede8a08443e4e18a95637b5fdd7316e71b
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-06-18 12:15:07 +0700

    Create 171-fun-zip().py

---
+++ python-with-road-map/7-built-in-function/171-fun-zip().py
@@ -0,0 +1,16 @@
+number_list = [1, 2, 3]
+str_list = ['one', 'two', 'three']
+
+# No iterables are passed
+result = zip()
+
+# Converting iterator to list
+result_list = list(result)
+print(result_list)
+
+# Two iterables are passed
+result = zip(number_list, str_list)
+
+# Converting iterator to set
+result_set = set(result)
+print(result_set)
