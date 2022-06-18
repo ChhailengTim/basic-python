fa6cd4590e40eb4e434f3b27df4815bcddef8b7a
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-06-18 12:15:10 +0700

    Create 172-fun-zip().py

---
+++ python-with-road-map/7-built-in-function/172-fun-zip().py
@@ -0,0 +1,16 @@
+numbersList = [1, 2, 3]
+str_list = ['one', 'two']
+numbers_tuple = ('ONE', 'TWO', 'THREE', 'FOUR')
+
+# Notice, the size of numbersList and numbers_tuple is different
+result = zip(numbersList, numbers_tuple)
+
+# Converting to set
+result_set = set(result)
+print(result_set)
+
+result = zip(numbersList, str_list, numbers_tuple)
+
+# Converting to set
+result_set = set(result)
+print(result_set)
