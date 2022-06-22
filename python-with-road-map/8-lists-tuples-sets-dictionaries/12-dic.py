2ee87ac8630fdeaeee14c9239cf767a4e33c61c8
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-23 00:06:23 +0700

    Create 12-dic.py

---
+++ python-with-road-map/8-lists-tuples-sets-dictionaries/12-dic.py
@@ -0,0 +1,7 @@
+dict1 = {"fruit1": "apple", "fruit2": "banana", "veg1": "tomato"}
+dict1.update({"veg2": "brinjal"})
+print(dict1)
+dict1.update({"veg3": "chilli"})
+print(dict1)
+dict1.pop("veg2")
+print(dict1)
