73e1f4923507e11da8e0dcdeaf08ad173d472eb1
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-23 00:06:40 +0700

    Create 6-list.py

---
+++ python-with-road-map/8-lists-tuples-sets-dictionaries/6-list.py
@@ -0,0 +1,9 @@
+list1 = ["hello", 1, 4, 8, "good"]
+print(list1)
+list1[0] = "morning"
+print(list1)
+print(list1[4])
+print(list1[-1])
+print(list1[1:4])
+list2 = ["apple", "mango", "banana"]
+print(list1 + list2)
