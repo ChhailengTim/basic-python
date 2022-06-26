8d1c16cca726f95f973109047413590e2f84663a
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-26 23:06:55 +0700

    Create 2-hash-table.py

---
+++ python-with-road-map/2-datastructures-and-algorithms/3-hash-table/2-hash-table.py
@@ -0,0 +1,6 @@
+# Declare a dictionary
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+dict['Age'] = 8; # update existing entry
+dict['School'] = "DPS School"; # Add new entry
+print ("dict['Age']: ", dict['Age'])
+print ("dict['School']: ", dict['School'])
