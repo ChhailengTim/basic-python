3431d201522fa9ba768bcea1643fe836f589535d
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-26 23:06:51 +0700

    Create 1-hash-table.py

---
+++ python-with-road-map/2-datastructures-and-algorithms/3-hash-table/1-hash-table.py
@@ -0,0 +1,6 @@
+# Declare a dictionary
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+
+# Accessing the dictionary with its key
+print("dict['Name']: ", dict['Name'])
+print("dict['Age']: ", dict['Age'])
