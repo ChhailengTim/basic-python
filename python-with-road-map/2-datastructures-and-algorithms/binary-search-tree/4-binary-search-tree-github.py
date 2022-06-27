d5f6a4b9399b5d46017bf17db4ba4eeaae60a90d
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-06-27 23:48:54 +0700

    Create 4-binary-search-tree-github.py

---
+++ python-with-road-map/2-datastructures-and-algorithms/binary-search-tree/4-binary-search-tree-github.py
@@ -0,0 +1,57 @@
+class TreeNode:
+    def __init__(self, key, val=None):
+        if val is None:
+            val = key
+
+        self.key = key
+        self.value = val
+        self.left = None
+        self.right = None
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    # Time Complexity:
+    # Space Complexity:
+    def add(self, key, value=None):
+        pass
+
+    # Time Complexity:
+    # Space Complexity:
+    def find(self, key):
+        pass
+
+    # Time Complexity:
+    # Space Complexity:
+    def inorder(self):
+        pass
+
+    # Time Complexity:
+    # Space Complexity:
+    def preorder(self):
+        pass
+
+    # Time Complexity:
+    # Space Complexity:
+    def postorder(self):
+        pass
+
+    # Time Complexity:
+    # Space Complexity:
+    def height(self):
+        pass
+
+    #   # Optional Method
+    #   # Time Complexity:
+    #   # Space Complexity:
+    def bfs(self):
+        pass
+
+    #   # Useful for printing
+    def to_s(self):
+        return f"{self.inorder()}"
+
+
+
