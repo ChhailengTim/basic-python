6d4577b791610ce7419c7d2d685f348e92dfe096
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-07-27 22:45:34 +0700

    Create 2-class.py

---
+++ python-with-road-map/3-advance-topics/1-oop/4-classes/2-class.py
@@ -0,0 +1,11 @@
+class Cat:
+    def __init__(self, name, color, age):
+        self.name = name
+        self.color = color
+        self.age = age
+
+
+c = Cat("Meow", "Brown", 3)
+print(c.name)
+print(c.color)
+print(c.age)
