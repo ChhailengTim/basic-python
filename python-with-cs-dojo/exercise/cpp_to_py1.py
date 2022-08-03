c8f0abeb33d7eb0d1ff15e0684356826c09e5278
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-08-03 21:50:32 +0700

    cpp to python

---
+++ python-with-cs-dojo/exercise/cpp_to_py1.py
@@ -0,0 +1,8 @@
+anglea=input("Input anglea: ")
+angleb=input("Input angleb: ")
+anglec=input("Input anglec: ")
+sum = anglea + angleb + anglec
+if sum==180:
+    print("It is a valid triangle.")
+else:
+    print("It is a invalid triangle.")
